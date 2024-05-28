d7aa90590c3f9f912bbcef7eae26a79b4f2275af
Lilibeth <lilibethocando@gmail.com>
2024-05-28 15:21:24 -0500

    Resolved virtual environment and directory structure issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+import os
+
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+app.config.from_object(Config)
+
+app.secret_key = os.environ.get('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+from . import models
 
 from app.routes import home, auth
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- my-react-app/src/components/SecondComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SecondComponent = () => {
-  return (
-    <div>SecondComponent</div>
-  )
-}
