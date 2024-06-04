de30976e0ed8fb94f4fc6c9ab91083e531f69cac
Lilibeth <lilibethocando@gmail.com>
2024-06-04 10:24:28 -0500

    Fixed templates/index.html

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,6 +2,13 @@ import os
 from flask import render_template
 from app import app
 
+# Get the filename of the generated JavaScript bundle
+js_bundle_dir = os.path.abspath(os.path.dirname(__file__)) + '/static/assets'
+js_bundle_filename = os.listdir(js_bundle_dir)[0]  # Assuming there's only one file in the directory
+
+# Extract the hash from the filename (assuming the format is "index-{hash}.js")
+bundle_hash = js_bundle_filename.split('-')[1]
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', bundle_hash=bundle_hash)
