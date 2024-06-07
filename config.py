f8a00f613b39c5190caeefc6d7a61c07e6a837fa
Jennifer-monteiro <70246891+Jennifer-monteiro@users.noreply.github.com>
2024-06-07 19:22:29 -0400

    packages removed

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional, to suppress a warning
     SESSION_TYPE = 'filesystem'
+
+
+
+#check it
